1e5c8d4d26eee23d9b92ce2448276b444546c624
Dios Vo <vtmn1212@gmail.com>
2025-10-07 23:52:05 +0700

    Transitions: Loading score without transitions

--- transitions/src/App.jsx
+++ transitions/src/App.jsx
@@ -1,3 +1,55 @@
+import { useEffect, useState } from 'react';
+import getScore from './getScore';
+import Score from './Score';
+
 export default function App() {
-  return <h1>Transitions</h1>;
+  const [isPending, setIsPending] = useState(true);
+  const [game, setGame] = useState(1);
+  const [score, setScore] = useState({ home: '-', away: '-' });
+
+  async function getNewScore(game) {
+    setIsPending(true);
+    setGame(game);
+    const newScore = await getScore(game);
+    setScore(newScore);
+    setIsPending(false);
+  }
+
+  useEffect(() => {
+    getNewScore(game);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Game {game}</h1>
+      <select
+        name="game"
+        id="game"
+        disabled={isPending}
+        onChange={(e) => getNewScore(e.target.value)}
+      >
+        <option value={1}>Game 1</option>
+        <option value={2}>Game 2</option>
+        <option value={3}>Game 3</option>
+        <option value={4}>Game 4</option>
+        <option value={5}>Game 5</option>
+        <option value={6}>Game 6</option>
+        <option value={7}>Game 7</option>
+      </select>
+      <div className={`loading-container ${isPending ? 'loading' : ''}`}>
+        <span className="spinner">⭕️</span>
+      </div>
+      <div>
+        <Score
+          isPending={isPending}
+          homeImage={score.homeImage}
+          homeName={score.homeImage}
+          awayImage={score.awayImage}
+          awayName={score.awayName}
+          home={score.home}
+          away={score.away}
+        />
+      </div>
+    </div>
+  );
 }
